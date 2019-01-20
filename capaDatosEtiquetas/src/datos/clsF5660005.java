5d97d9417eacc431032333f32f59e95a2fcdd8af
Emiliano <emiliano.guido@gmail.com>
2019-01-19 21:26:06 -0300

    k

---
+++ capaDatosEtiquetas/src/datos/clsF5660005.java
@@ -0,0 +1,244 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package datos;
+
+/**
+ *
+ * @author ge0022
+ */
+public class clsF5660005 {
+    
+    private String ETAA12;
+    private String ETLITM;
+    private String ETCITM;
+    private float  ETASTF;
+    private String ETAA15;
+    private float  ETDOCO;
+    private String ETAA20;
+    private String ETAMCU;
+    private String ETLOCN;
+    private String ETCMCU;
+    private String ETDS20;
+    private String ETA301;
+    private String ETAA03;
+    private String ETEV01;
+    private String ETQ20;
+    private String ETMCU;
+    private long   ETADDJ;
+    private String ETUSER;
+    private String ETFLAG;
+    private String ETFLA;
+    private String ETMUSE;
+    private String ETFBL;
+
+    public clsF5660005()
+    {
+    }
+    
+    public clsF5660005(String ETAA12, String ETLITM, String ETCITM, float ETASTF, String ETAA15, float ETDOCO, String ETAA20, String ETAMCU, String ETLOCN, String ETCMCU, String ETDS20, String ETA301, String ETAA03, String ETEV01, String ETQ20, String ETMCU, long ETADDJ, String ETUSER, String ETFLAG, String ETFLA, String ETMUSE, String ETFBL) {
+        this.ETAA12 = ETAA12;
+        this.ETLITM = ETLITM;
+        this.ETCITM = ETCITM;
+        this.ETASTF = ETASTF;
+        this.ETAA15 = ETAA15;
+        this.ETDOCO = ETDOCO;
+        this.ETAA20 = ETAA20;
+        this.ETAMCU = ETAMCU;
+        this.ETLOCN = ETLOCN;
+        this.ETCMCU = ETCMCU;
+        this.ETDS20 = ETDS20;
+        this.ETA301 = ETA301;
+        this.ETAA03 = ETAA03;
+        this.ETEV01 = ETEV01;
+        this.ETQ20 = ETQ20;
+        this.ETMCU = ETMCU;
+        this.ETADDJ = ETADDJ;
+        this.ETUSER = ETUSER;
+        this.ETFLAG = ETFLAG;
+        this.ETFLA = ETFLA;
+        this.ETMUSE = ETMUSE;
+        this.ETFBL = ETFBL;
+    }
+    
+    
+    public String getETAA12() {
+        return ETAA12;
+    }
+
+    public void setETAA12(String ETAA12) {
+        this.ETAA12 = ETAA12;
+    }
+
+    public String getETLITM() {
+        return ETLITM;
+    }
+
+    public void setETLITM(String ETLITM) {
+        this.ETLITM = ETLITM;
+    }
+
+    public String getETCITM() {
+        return ETCITM;
+    }
+
+    public void setETCITM(String ETCITM) {
+        this.ETCITM = ETCITM;
+    }
+
+    public float getETASTF() {
+        return ETASTF;
+    }
+
+    public void setETASTF(float ETASTF) {
+        this.ETASTF = ETASTF;
+    }
+
+    public String getETAA15() {
+        return ETAA15;
+    }
+
+    public void setETAA15(String ETAA15) {
+        this.ETAA15 = ETAA15;
+    }
+
+    public float getETDOCO() {
+        return ETDOCO;
+    }
+
+    public void setETDOCO(float ETDOCO) {
+        this.ETDOCO = ETDOCO;
+    }
+
+    public String getETAA20() {
+        return ETAA20;
+    }
+
+    public void setETAA20(String ETAA20) {
+        this.ETAA20 = ETAA20;
+    }
+
+    public String getETAMCU() {
+        return ETAMCU;
+    }
+
+    public void setETAMCU(String ETAMCU) {
+        this.ETAMCU = ETAMCU;
+    }
+
+    public String getETLOCN() {
+        return ETLOCN;
+    }
+
+    public void setETLOCN(String ETLOCN) {
+        this.ETLOCN = ETLOCN;
+    }
+
+    public String getETCMCU() {
+        return ETCMCU;
+    }
+
+    public void setETCMCU(String ETCMCU) {
+        this.ETCMCU = ETCMCU;
+    }
+
+    public String getETDS20() {
+        return ETDS20;
+    }
+
+    public void setETDS20(String ETDS20) {
+        this.ETDS20 = ETDS20;
+    }
+
+    public String getETA301() {
+        return ETA301;
+    }
+
+    public void setETA301(String ETA301) {
+        this.ETA301 = ETA301;
+    }
+
+    public String getETAA03() {
+        return ETAA03;
+    }
+
+    public void setETAA03(String ETAA03) {
+        this.ETAA03 = ETAA03;
+    }
+
+    public String getETEV01() {
+        return ETEV01;
+    }
+
+    public void setETEV01(String ETEV01) {
+        this.ETEV01 = ETEV01;
+    }
+
+    public String getETQ20() {
+        return ETQ20;
+    }
+
+    public void setETQ20(String ETQ20) {
+        this.ETQ20 = ETQ20;
+    }
+
+    public String getETMCU() {
+        return ETMCU;
+    }
+
+    public void setETMCU(String ETMCU) {
+        this.ETMCU = ETMCU;
+    }
+
+    public long getETADDJ() {
+        return ETADDJ;
+    }
+
+    public void setETADDJ(long ETADDJ) {
+        this.ETADDJ = ETADDJ;
+    }
+
+    public String getETUSER() {
+        return ETUSER;
+    }
+
+    public void setETUSER(String ETUSER) {
+        this.ETUSER = ETUSER;
+    }
+
+    public String getETFLAG() {
+        return ETFLAG;
+    }
+
+    public void setETFLAG(String ETFLAG) {
+        this.ETFLAG = ETFLAG;
+    }
+
+    public String getETFLA() {
+        return ETFLA;
+    }
+
+    public void setETFLA(String ETFLA) {
+        this.ETFLA = ETFLA;
+    }
+
+    public String getETMUSE() {
+        return ETMUSE;
+    }
+
+    public void setETMUSE(String ETMUSE) {
+        this.ETMUSE = ETMUSE;
+    }
+
+    public String getETFBL() {
+        return ETFBL;
+    }
+
+    public void setETFBL(String ETFBL) {
+        this.ETFBL = ETFBL;
+    }
+    
+}
