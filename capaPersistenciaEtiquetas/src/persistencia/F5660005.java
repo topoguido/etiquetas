2e84dcd546c1aad3fcc077534e3cf821f3ce80e8
ge0022 <ge0022@WAR5CG6060SP8.soam.pilkington.net>
2019-01-22 13:49:02 -0300

    b

--- capaPersistenciaEtiquetas/src/persistencia/F5660005.java
+++ capaPersistenciaEtiquetas/src/persistencia/F5660005.java
@@ -15,25 +15,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-
+import com.microsoft.sqlserver.jdbc.*;
 public class F5660005 {
-    
-Connection co;    
-Statement st;
 
-String host="SBRSQL31251";
-String user="GE0022";
-String password="topoguido001";
+	/*String host="SBRSQL31251";
+	String user="GE0022";
+	String password="topoguido001";*/
 
- public ArrayList getEtiqueta(String etiqueta)
+	
+
+ public ArrayList<Object> getEtiqueta(String etiqueta)
    {
-        ArrayList resultado = new ArrayList();
+        ArrayList<Object>  resultado = new ArrayList<>();
+        Statement st;
         ResultSet res;
         try{
 
-               Class.forName("com.microsoft.sqlserver.jdbc.Driver");
-               Connection co = DriverManager.getConnection(host, user, password);
-               st=co.createStatement();
+               Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+               /*Connection co = DriverManager.getConnection(host, user, password);*/
+               String connectionUrl = "jdbc:sqlserver://SBRSQL31251;database=JDE_CRP;integratedSecurity=true";
+               /*String connectionUrl = "jdbc:sqlserver://;servername=SBRSQL31251;integratedSecurity=true;authenticationScheme=JavaKerberos";*/
+               Connection conect = DriverManager.getConnection(connectionUrl);
+               
+              /* SQLServerDataSource ds = new SQLServerDataSource();  
+               ds.setUser("soam\\ge0022");  
+               ds.setPassword("topoguido001");  
+               ds.setServerName("SBRSQL31251");  
+               ds.setPortNumber(1433);
+               ds.setDatabaseName("JDE_CRP");  
+               Connection conect = ds.getConnection(); */
+               
+               st = conect.createStatement();
 
                String sentencia="SELECT * FROM CRPDTA.F5660005 WHERE ETAA12 = ('"+etiqueta+"')";
                res = st.executeQuery(sentencia);
